c85186c57413758b5140b955c37214409362cc0c
Greg <60011793+gregtuc@users.noreply.github.com>
2020-05-30 13:47:57 -0400

    Runtime Error - Pushes had to be factored in

--- winloss.py
+++ winloss.py
@@ -2,13 +2,22 @@ import csv
 
 f = open('blkjckhands.csv')
 csv_f = csv.reader(f)
-wincount = 0;
+wincount = 0
+pushcount = 0
 
 for row in csv_f:
     rowstring = ''.join(row[15])
     if rowstring == "Win":
-        wincount = wincount + 1;
-        print(wincount)
+        wincount = wincount + 1
+    if rowstring == "Push":
+        pushcount = pushcount + 1
 
+#Print Wins, Losses, and Pushes
 print("Total Wins: ", wincount)
+print("Total Losses: ", 900000-(wincount+pushcount))
+print("Total Pushes: ", pushcount)
+
+#Print Win Rate, Loss Rate, Push Rate
 print("Total Win Rate: ", wincount/900000)
+print("Total Loss Rate: ", (900000-wincount)/900000)
+print("Total Push Rate: ", pushcount/900000)
